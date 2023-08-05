f59d83233b310f662d9ec7882271212436b6f6ba
Salma Shaik <111558265+SalmaRivaan@users.noreply.github.com>
2023-08-05 13:16:29 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,14 @@ import 'firebase/compat/auth';
 
 
 const firebaseApp = firebase.initializeApp({  
-
-    apiKey: "AIzaSyAWWqR5NFPaw-_70fuYYE31jT9UsAARYPg",
-  authDomain: "fir-b6011.firebaseapp.com",
-  projectId: "fir-b6011",
-  storageBucket: "fir-b6011.appspot.com",
-  messagingSenderId: "618294940565",
-  appId: "1:618294940565:web:3e15687056803635b79c9c",
+ apiKey: "AIzaSyAfU0gEQtSHQEIywoAIEUKvgV3LPWFQvNw",
+  authDomain: "fir-9587a.firebaseapp.com",
+  projectId: "fir-9587a",
+  storageBucket: "fir-9587a.appspot.com",
+  messagingSenderId: "157792477812",
+  appId: "1:157792477812:web:eeb8bde8760870ac3ea721"
+   
+ 
   measurementId: "G-3VVM8SL1DM"
 });
 
